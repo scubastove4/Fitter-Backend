225215f1b16547e18732bcbc46511c9d50b549e8
Steve Morello <smorello4@gmail.com>
2022-08-11 11:22:34 -0400

    create signup and login controllers

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,16 +4,21 @@ const middleware = require('../middleware')
 const SignUp = async (req, res) => {
   try {
     const { name, age, location, username, email, password } = req.body
-    let passwordDigest = await middleware.hashPassword(password)
-    const newUser = await User.create(
-      name,
-      age,
-      location,
-      username,
-      email,
-      passwordDigest
-    )
-    res.send(newUser)
+    let existingUser = await User.findOne({ where: { username: username } })
+    if (existingUser) {
+      res.send({ msg: `${existingUser.username} is taken.` })
+    } else {
+      let passwordDigest = await middleware.hashPassword(password)
+      const newUser = await User.create(
+        name,
+        age,
+        location,
+        username,
+        email,
+        passwordDigest
+      )
+      res.send(newUser)
+    }
   } catch (e) {
     throw e
   }
@@ -21,12 +26,27 @@ const SignUp = async (req, res) => {
 
 const Login = async (req, res) => {
   try {
-    const { username } = req.body
+    const user = await User.findOne({
+      where: { username: req.body.username }
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(req.body.password, user.passwordDigest))
+    ) {
+      let payload = {
+        id: user.id,
+        email: user.email,
+        username: user.username
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
   } catch (e) {
     throw e
   }
 }
 
 module.exports = {
-  SignUp
+  SignUp,
+  Login
 }
