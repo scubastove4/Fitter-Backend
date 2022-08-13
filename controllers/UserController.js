30d51aa0d20c09794fc4b96a9a1ae5e1a46e54cb
Steve Morello <smorello4@gmail.com>
2022-08-13 13:11:12 -0400

    add location to payload

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -36,7 +36,8 @@ const Login = async (req, res) => {
       let payload = {
         id: user.id,
         email: user.email,
-        username: user.username
+        username: user.username,
+        location: user.location
       }
       let token = middleware.createToken(payload)
       return res.send({ user: payload, token })
