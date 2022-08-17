65b5b6b55c669f67127d46c81cde73882ab4b322
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-08-17 17:47:25 -0400

    working on upload image migration, added id attribute to comment model in GetFeatById

--- controllers/FeatController.js
+++ controllers/FeatController.js
@@ -51,7 +51,7 @@ const GetFeatById = async (req, res) => {
           model: Comment,
           as: 'comment_list',
           order: [['createdAt', 'DESC']],
-          attributes: ['description'],
+          attributes: ['description', 'id'],
           include: [
             {
               model: User,
